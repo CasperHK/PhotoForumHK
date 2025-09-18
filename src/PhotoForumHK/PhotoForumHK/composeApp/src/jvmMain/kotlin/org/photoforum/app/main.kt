c2f44d054fe16d018b1247049fc0483d5620b14a
Casper LI <casperli@hko.hksarg>
2025-09-18 12:48:08 +0800

    Add cross-platform framework: Compose Multiplatform

---
+++ src/PhotoForumHK/PhotoForumHK/composeApp/src/jvmMain/kotlin/org/photoforum/app/main.kt
@@ -0,0 +1,13 @@
+package org.photoforum.app
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "PhotoForumHK",
+    ) {
+        App()
+    }
+}
